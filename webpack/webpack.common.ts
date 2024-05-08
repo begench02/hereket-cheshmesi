7d6a1d469f4d939d003e9b1b2b468211cff15ac5
Begench <begenchgeldyev@gmail.com>
2024-05-09 01:10:49 +0700

    feat: rename header

--- webpack/webpack.common.ts
+++ webpack/webpack.common.ts
@@ -42,7 +42,6 @@ export const webpackCommon: Configuration = {
 					{ loader: 'postcss-loader' },
 					{
 						loader: 'sass-loader',
-
 						options: {
 							sassOptions: {
 								includePaths: [path.resolve(__dirname, '../src/')],
